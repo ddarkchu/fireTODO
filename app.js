7fc519c2f19af8a4211fcfa42cf6022dfba64877
ddarkchu <ddarkchu@gmail.com>
2017-06-07 15:59:47 +0900

    add log process.

--- app.js
+++ app.js
@@ -1,36 +1,27 @@
 var admin = require("firebase-admin");
-var serviceAccount = require("../config/adminKey.json");
+var serviceAccount = require("./config/adminKey.json");
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://todo-47c04.firebaseio.com"
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://todo-47c04.firebaseio.com"
 });
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
+
+// write log module
+var wins = require('./log.js')
 var logger = require('morgan');
+// write log module //
+
+
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var rfs = require('rotating-file-stream');
-var fs = require('fs');
 
 var app = express();
-var logDirectory = path.join(__dirname, 'log');
 
-
-
-// ensure log directory exists
-fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory)
-
-// create a rotating write stream
-var accessLogStream = rfs('access.log', {
-  interval: '1d', // rotate daily
-  path: logDirectory
-})
-
-// setup the logger
-
+var config = require('./config.json');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -40,10 +31,14 @@ app.set('view engine', 'pug');
 
 
 // app.use(logger('dev'));
-app.use(logger('combined', {stream: accessLogStream}));
+
+app.use(logger('combined', {
+    stream: wins.stream
+}));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-	extended: false
+    extended: false
 }));
 
 app.use(cookieParser());
@@ -52,29 +47,31 @@ app.use('/', routes);
 app.use('/users', users);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-	var err = new Error('Not Found');
-	err.status = 404;
-	next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 // error handlers
 // development error handler
 // will print stacktrace
-if(app.get('env') === 'development') {
-	app.use(function(err, req, res, next) {
-		res.status(err.status || 500);
-		res.render('error', {
-			message: err.message,
-			error: err
-		});
-	});
-}
-// production error handler
-// no stacktraces leaked to user
+
 app.use(function(err, req, res, next) {
-	res.status(err.status || 500);
-	res.render('error', {
-		message: err.message,
-		error: {}
-	});
+    wins.error(err);
+    var result = {
+        message: err.message,
+        error: ""
+    };
+    if (config && config.env === 'development') {
+        result.error = err;
+    }
+
+    res.status(err.status || 500);
+    res.render('error', result);
 });
+
+process.on('uncaughtException', function(err) {
+    //죽기 방지용 루틴.
+    wins.error(err);
+});
+
 module.exports = app;
---
+++ log.js
@@ -0,0 +1,33 @@
+var path = require('path');
+var fs = require('fs');
+var winston = require('winston');
+var DailyRotateFile = require('winston-daily-rotate-file')
+
+var logDirectory = path.join(__dirname, 'log');
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory)
+
+var wins = new(winston.Logger)({
+    transports: [
+        new(winston.transports.Console)(),
+        new(require('winston-daily-rotate-file'))({
+            filename: path.join(logDirectory, 'all-logs.log'),
+            datePattern: 'yyyy-MM-dd.',
+            prepend: true,
+            // level: process.env.ENV === 'development' ? 'debug' : 'info'
+        }),
+        new(require('winston-daily-rotate-file'))({
+            filename: path.join(logDirectory, 'exceptions.log'),
+            datePattern: 'yyyy-MM-dd.',
+            prepend: true,
+            level: 'error',
+            name: 'error'
+            // level: process.env.ENV === 'development' ? 'debug' : 'info'
+        })
+    ]
+});
+wins.stream = {
+    write: function(message, encoding) {
+        wins.info(message);
+    }
+};
+exports = wins;
